10e1377ac2931b9aa16ee63769f30613926db0af
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-02-05 23:19:47 +0530

    feat: shows error message for unauthenticated users

--- app/projects/pages/projects/[projectId].tsx
+++ app/projects/pages/projects/[projectId].tsx
@@ -1,7 +1,16 @@
-import { Center, Flex, Grid, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Grid,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import ProtectedLayout from "app/layouts/ProtectedLayout";
 import { BlitzPage, dynamic } from "blitz";
 import React, { Suspense, useEffect } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const LazyContentArea = dynamic(
   () =>
@@ -66,13 +75,25 @@ const ShowProjectPage: BlitzPage = () => {
 
   return (
     <Flex flexDir="column" w="100%" h="calc(100vh - 80px)">
-      <Suspense fallback={<Spinner />}>
-        <Grid templateColumns={["1fr 8fr 3fr"]} w="100%" h="100%">
-          <LazyFilesSidebar />
-          <LazyContentArea />
-          <LazyDetailsSidebar />
-        </Grid>
-      </Suspense>
+      <ErrorBoundary
+        fallbackRender={({ error }) => {
+          return (
+            <Container maxW="6xl" centerContent p={8}>
+              <Box p={8} bg="gray.100" rounded="md">
+                <Text fontWeight="bold">{error.message}</Text>
+              </Box>
+            </Container>
+          );
+        }}
+      >
+        <Suspense fallback={<Spinner />}>
+          <Grid templateColumns={["1fr 8fr 3fr"]} w="100%" h="100%">
+            <LazyFilesSidebar />
+            <LazyContentArea />
+            <LazyDetailsSidebar />
+          </Grid>
+        </Suspense>
+      </ErrorBoundary>
     </Flex>
   );
 };
