eaaecca45277b1d0af6acde8f60321a92926ae24
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-02-16 11:36:03 +0530

    feat: adds option to delete records

--- app/projects/components/DetailsSidebar/ProjectSettings.tsx
+++ app/projects/components/DetailsSidebar/ProjectSettings.tsx
@@ -1,14 +1,19 @@
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack } from "@chakra-ui/react";
 import ErrorState from "app/components/ErrorState";
 import { useCurrentUser } from "app/hooks/useCurrentUser";
 import ProjectStateRadio from "app/projects/components/DetailsSidebar/ProjectStateRadio";
-import { Link, useParam } from "blitz";
+import deleteProject from "app/projects/mutations/deleteProject";
+import { Link, useMutation, useParam, useRouter } from "blitz";
 import React, { FC } from "react";
 import { MdLock } from "react-icons/md";
 
 const ProjectSettings: FC = () => {
+  const router = useRouter();
   const projectId = useParam("projectId", "string");
   const currentUser = useCurrentUser();
+  const [deleteProjectMutation, { isLoading, isError }] = useMutation(
+    deleteProject
+  );
 
   if (!currentUser) {
     return (
@@ -35,11 +40,36 @@ const ProjectSettings: FC = () => {
           <Heading size="sm">Modify project</Heading>
         </Box>
         <Box p={4}>
-          <Link href={`/projects/${projectId}/edit`} passHref>
-            <Button as="a" colorScheme="blue" size="lg" w="100%">
-              Edit Project
+          <HStack spacing={4}>
+            <Link href={`/projects/${projectId}/edit`} passHref>
+              <Button
+                as="a"
+                colorScheme="blue"
+                size="lg"
+                w="100%"
+                isLoading={isLoading}
+              >
+                Edit project
+              </Button>
+            </Link>
+            <Button
+              colorScheme="red"
+              size="lg"
+              isLoading={isLoading}
+              w="100%"
+              onClick={async () => {
+                if (window.confirm("This will be deleted")) {
+                  await deleteProjectMutation({
+                    where: { id: projectId },
+                  });
+
+                  router.push("/projects");
+                }
+              }}
+            >
+              Delete project
             </Button>
-          </Link>
+          </HStack>
         </Box>
       </Box>
     );
